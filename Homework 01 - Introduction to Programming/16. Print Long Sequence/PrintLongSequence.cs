9635bdd35c9df1e6b1bac5c1f9ff8d2dd840f3e9
FilipFilipov <murela_alba@abv.bg>
2015-01-18 00:53:19 +0200

    Add the last exercise.

---
+++ Homework 01 - Introduction to Programming/16. Print Long Sequence/PrintLongSequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class PrintLongSequence
+{
+    static void Main()
+    {
+        int num = 3;
+        Console.Write(2);
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Write(", {0}", num);
+            if (num > 0)
+            {
+                num = (num * (-1)) - 1;
+            }
+            else
+            {
+                num = (num * (-1)) + 1;
+            }
+        }
+        Console.WriteLine();
+    }
+}
